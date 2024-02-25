ee713fbd63ec8b17f2f05af4628168bcfed6863d
luisferreiraa <luis.c.ferreira.prt_a@msft.cesae.pt>
2024-02-25 21:45:21 +0000

    fichas gpt

---
+++ Java/Fichas Java/src/fichas_gpt/Exercício01.java
@@ -0,0 +1,15 @@
+package fichas_gpt;
+
+public class Exercício01 {
+
+    public static void main(String[] args) {
+
+        // Escreva um programa que imprima os números de 1 a 10.
+
+
+        for (int num = 1; num <= 10; num++) {
+
+            System.out.println(num);
+        }
+    }
+}
