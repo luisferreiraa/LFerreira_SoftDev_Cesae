826734c9181e24ad2cbccca468ba18ba88a51b7b
Luís Ferreira <luis.c.ferreira.prt_a@msft.cesae.pt>
2024-02-27 16:54:11 +0000

    java gpt

--- Java/Fichas Java/src/fichas_gpt/Exercício05.java
+++ Java/Fichas Java/src/fichas_gpt/Exercício05.java
@@ -4,6 +4,20 @@ public class Exercício05 {
 
     public static void main(String[] args) {
 
+        int num1 = 0;
+        int num2 = 1;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+        for (int i = 2; i<10 ; i++) {
+            int nextNum = num1 + num2;
+            System.out.println(nextNum);
+
+            num1 = num2;
+            num2 = nextNum;
+        }
+
 
     }
 }
